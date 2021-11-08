6a927e218f3476cd93156b45945f041d406eb599
Sven Graziani <SGraziani@anexia-it.com>
2021-11-08 14:02:50 +0100

    add tests, update postBodyIdentifer

--- src/lib/utils/request-body-serializer.ts
+++ src/lib/utils/request-body-serializer.ts
@@ -1,11 +1,11 @@
 import {HttpRequest} from "@angular/common/http";
 
 export function requestBodySerializer(request: HttpRequest<any>, bodyParams: string[]): string {
-  const postBody = [];
+  const postBody: string[] = [];
   bodyParams.forEach(param => {
     if (request.body.hasOwnProperty(param)) {
       postBody.push(request.body[param]);
     }
   });
-  return [urlIdentifier, postBody.join('_')].join('_');
+  return [postBody.join('_')].join('_');
 }
